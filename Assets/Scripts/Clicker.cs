33d2a0c1c7baf418f41c7d537a6d741c9921c56b
zipals38 <34783137+zipals38@users.noreply.github.com>
2020-07-15 18:41:57 +0900

    게임 수정
    수정 및 임시UI 추가

---
+++ Assets/Scripts/Clicker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clicker : MonoBehaviour
+{
+    public int clickNum;
+
+    public void OnScreenClicked()
+    {
+        clickNum += 1;
+        Debug.Log(clickNum);
+    }
+}
---
+++ Assets/Scripts/MainPageSwiper.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MainPageSwiper : MonoBehaviour, IDragHandler, IEndDragHandler, IBeginDragHandler
+{
+    private Vector3 panelLocation;
+    public float percentThreshold = 0.2f;
+    public float easing = 0.5f;
+    public int totalPages = 1;
+    private int currentPage = 1;
+    public bool isOnDrag = false;
+
+    void Start()
+    {
+        panelLocation = transform.position;
+    }
+
+    public void OnBeginDrag(PointerEventData data)   //드래그 시작 - 춘배 걷는 애니메이션 출력
+    {
+        isOnDrag = true;
+    }
+
+    public void OnDrag(PointerEventData data)   //드래그 중
+    {
+        float difference = data.pressPosition.x - data.position.x;
+        transform.position = panelLocation - new Vector3(difference, 0, 0);
+    }
+
+    public void OnEndDrag(PointerEventData data)   //드래그 끝 - 춘배 가만히 있는 애니메이션 출력
+    {
+        float percentage = (data.pressPosition.x - data.position.x) / Screen.width;
+        if (Mathf.Abs(percentage) >= percentThreshold)
+        {
+            Vector3 newLocation = panelLocation;
+            if (percentage > 0 && currentPage < totalPages)
+            {
+                currentPage++;
+                newLocation += new Vector3(-Screen.width, 0, 0);
+            }
+            else if (percentage < 0 && currentPage > 1)
+            {
+                currentPage--;
+                newLocation += new Vector3(Screen.width, 0, 0);
+            }
+            StartCoroutine(SmoothMove(transform.position, newLocation, easing));
+            panelLocation = newLocation;
+        }
+        else
+        {
+            StartCoroutine(SmoothMove(transform.position, panelLocation, easing));
+        }
+        isOnDrag = false;
+    }
+
+    IEnumerator SmoothMove(Vector3 startpos, Vector3 endpos, float seconds)
+    {
+        float t = 0f;
+        while (t <= 1.0)
+        {
+            t += Time.deltaTime / seconds;
+            transform.position = Vector3.Lerp(startpos, endpos, Mathf.SmoothStep(0f, 1f, t));
+            yield return null;
+        }
+    }
+
+}
