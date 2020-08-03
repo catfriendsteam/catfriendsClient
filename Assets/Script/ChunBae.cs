3606959d00bcd2a8ca1e3e29830a96f80a375b1b
YerimJung <60317213+YerimJung@users.noreply.github.com>
2020-08-03 21:22:10 +0900

    춘배버튼 눌렀을때
    얼굴눌렀을때 서브메뉴 뜨고 X누르면 닫히게 했습니담

---
+++ Assets/Script/ChunBae.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunBae : MonoBehaviour
+{
+    public GameObject menu_chunbae;
+    public void ChunBaeButton()
+    {
+        menu_chunbae.SetActive(true);
+    }
+    public void XButton()
+    {
+        menu_chunbae.SetActive(false);
+    }
+}
