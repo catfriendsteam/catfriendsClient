3e26c1ea6127a407f3fa138ae03d5fe706788549
GwonHyunWoo <201411238@sangmyung.kr>
2020-08-04 13:51:39 +0900

    각 화면마다 게이지 상승 적용

--- Assets/Script/ButtonMng.cs
+++ Assets/Script/ButtonMng.cs
@@ -20,26 +20,64 @@ public class ButtonMng : MonoBehaviour
     public Slider slider;
     //치킨 슬라이더
     public Slider slider_chicken;
-
+    //곱창 슬라이더
+    public Slider slider_Gobchang;
+    //헬스 슬라이더
+    public Slider slider_Health;
+    //냥냐랜드 슬라이더
+    public Slider slider_Land;
 
     public void IncreaseGage()
     {
+        //카페 게이지 상승
         if (nestedScrollMng.targetIndex >= 0 && nestedScrollMng.targetIndex < 1)
         {
             slider.value += 1;   //게이지 1%씩 증가
             gageMng.isClicked = true;
             gageMng.timer = 0.0f;
+
             statusMng.Touch_IncreaseMoney();
         }
 
-
+        //치킨 게이지 상승
         else if (nestedScrollMng.targetIndex >= 1 && nestedScrollMng.targetIndex < 2)
         {
             slider_chicken.value += 1;   //게이지 1%씩 증가
             gageMng.isClicked_chicken = true;
             gageMng.timer_chicken = 0.0f;
+
             statusMng.Touch_IncreaseMoney();
         }
+        //곱창 게이지 상승
+        else if (nestedScrollMng.targetIndex >= 2 && nestedScrollMng.targetIndex < 3)
+        {
+            slider_Gobchang.value += 1;   //게이지 1%씩 증가
+            gageMng.isClicked_Gobchang = true;
+            gageMng.timer_Gobchang = 0.0f;
+
+            statusMng.Touch_IncreaseMoney();
+        }
+
+        //헬스 게이지 상승
+        else if (nestedScrollMng.targetIndex >= 3 && nestedScrollMng.targetIndex < 4)
+        {
+            slider_Health.value += 1;   //게이지 1%씩 증가
+            gageMng.isClicked_Health = true;
+            gageMng.timer_Health = 0.0f;
+
+            statusMng.Touch_IncreaseMoney();
+        }
+        //냥냐랜드 게이지 상승
+        else if (nestedScrollMng.targetIndex >= 4 && nestedScrollMng.targetIndex < 5)
+        {
+            slider_Land.value += 1;   //게이지 1%씩 증가
+            gageMng.isClicked_Land = true;
+            gageMng.timer_Land = 0.0f;
+
+            statusMng.Touch_IncreaseMoney();
+        }
+
+
     }
 
 
--- Assets/Script/GageMng.cs
+++ Assets/Script/GageMng.cs
@@ -23,7 +23,23 @@ public class GageMng : MonoBehaviour
     public float timer_chicken;
     public Text gageText_chicken;
 
+    //곱창집 게이지 선언
+    public Slider slider_Gobchang;
+    public bool isClicked_Gobchang;
+    public float timer_Gobchang;
+    public Text gageText_Gobchang;
 
+    //헬스장 게이지 선언
+    public Slider slider_Health;
+    public bool isClicked_Health;
+    public float timer_Health;
+    public Text gageText_Health;
+
+    //냥냐랜드 게이지 선언
+    public Slider slider_Land;
+    public bool isClicked_Land;
+    public float timer_Land;
+    public Text gageText_Land;
 
     void Awake()
     {
@@ -38,6 +54,24 @@ public class GageMng : MonoBehaviour
         SetGage_chicken(0);
         isClicked_chicken = false;
         timer_chicken = 0.0f;
+
+        //곱창집 선언
+        SetMaxGage_Gobchang(100);
+        SetGage_Gobchang(0);
+        isClicked_Gobchang = false;
+        timer_Gobchang = 0.0f;
+
+        //헬스장 선언
+        SetMaxGage_Health(100);
+        SetGage_Health(0);
+        isClicked_Health = false;
+        timer_Health = 0.0f;
+
+        //냥냐랜드 선언
+        SetMaxGage_Land(100);
+        SetGage_Land(0);
+        isClicked_Land = false;
+        timer_Land = 0.0f;
 
     }
 
@@ -80,7 +114,55 @@ public class GageMng : MonoBehaviour
         }
 
 
+        //곱창집 업데이트 문
 
+        gageText_Gobchang.text = Mathf.CeilToInt(slider_Gobchang.value).ToString();   // 게이지 숫자 텍스트 표시(실수를 정수로 문자화)
+        if (slider_Gobchang.value > 0)
+        {
+            if (isClicked_Gobchang == true)
+            {
+                timer_Gobchang += Time.deltaTime;
+            }
+
+            if (timer_Gobchang >= 2.0f)  // 2초동안 게이지버튼 클릭 안하면 게이지 제거
+            {
+                isClicked_Gobchang = false;
+                slider_Gobchang.value -= Time.deltaTime * 10;   // 초당 10씩 게이지 제거
+            }
+        }
+
+        //헬스장 업데이트 문
+
+        gageText_Health.text = Mathf.CeilToInt(slider_Health.value).ToString();   // 게이지 숫자 텍스트 표시(실수를 정수로 문자화)
+        if (slider_Health.value > 0)
+        {
+            if (isClicked_Health == true)
+            {
+                timer_Health += Time.deltaTime;
+            }
+
+            if (timer_Health >= 2.0f)  // 2초동안 게이지버튼 클릭 안하면 게이지 제거
+            {
+                isClicked_Health = false;
+                slider_Health.value -= Time.deltaTime * 10;   // 초당 10씩 게이지 제거
+            }
+        }
+        //냥냐랜드 업데이트 문
+
+        gageText_Land.text = Mathf.CeilToInt(slider_Land.value).ToString();   // 게이지 숫자 텍스트 표시(실수를 정수로 문자화)
+        if (slider_Land.value > 0)
+        {
+            if (isClicked_Land == true)
+            {
+                timer_Land += Time.deltaTime;
+            }
+
+            if (timer_Land >= 2.0f)  // 2초동안 게이지버튼 클릭 안하면 게이지 제거
+            {
+                isClicked_Land = false;
+                slider_Land.value -= Time.deltaTime * 10;   // 초당 10씩 게이지 제거
+            }
+        }
 
 
 
@@ -117,4 +199,44 @@ public class GageMng : MonoBehaviour
         slider_chicken.value = gageValue;
     }
 
+    //곱창집 함수
+
+
+
+    public void SetMaxGage_Gobchang(int gageValue)   // 게이지 최대값 설정 함수
+    {
+        slider_Gobchang.maxValue = gageValue;
+        slider_Gobchang.value = gageValue;
+    }
+
+    public void SetGage_Gobchang(int gageValue)   // 게이지 초기값 설정 함수
+    {
+        slider_Gobchang.value = gageValue;
+    }
+
+    //헬스장 함수
+    
+    public void SetMaxGage_Health(int gageValue)   // 게이지 최대값 설정 함수
+    {
+        slider_Health.maxValue = gageValue;
+        slider_Health.value = gageValue;
+    }
+
+    public void SetGage_Health(int gageValue)   // 게이지 초기값 설정 함수
+    {
+        slider_Health.value = gageValue;
+    }   
+
+    //냥냐랜드 함수
+
+    public void SetMaxGage_Land(int gageValue)   // 게이지 최대값 설정 함수
+    {
+        slider_Land.maxValue = gageValue;
+        slider_Land.value = gageValue;
+    }
+
+    public void SetGage_Land(int gageValue)   // 게이지 초기값 설정 함수
+    {
+        slider_Land.value = gageValue;
+    }
 }
