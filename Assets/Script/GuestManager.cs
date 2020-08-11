ad9093689efe8df6d2cc5e2b23c913ac9b1feb34
GwonHyunWoo <201411238@sangmyung.kr>
2020-08-12 02:13:29 +0900

    행인 젠
    ㄴㄴ

--- Assets/Script/GuestManager.cs
+++ Assets/Script/GuestManager.cs
@@ -11,6 +11,8 @@ public class GuestManager : MonoBehaviour
     public float maxSpawnDelay;
     public float curSpawnDelay;
 
+    public Vector3 recieveSpawnVector;
+
     void Update()
     {
         curSpawnDelay += Time.deltaTime;
@@ -19,8 +21,8 @@ public class GuestManager : MonoBehaviour
         {
             SpawnEnemy();
 
-            //랜덤하게 딜래이를 준다.
-            maxSpawnDelay = Random.Range(5f, 8f);
+            //랜덤하게 딜래이를 준다. 숫자~ 숫자
+            maxSpawnDelay = Random.Range(2f, 3f);
 
             curSpawnDelay = 0;
         }
@@ -30,16 +32,31 @@ public class GuestManager : MonoBehaviour
     {
         //0부터 시작하고, 그리고 랜덤한게 2개 있다.
         int ranEnemy = Random.Range(0, 2);
-        //0부터 시작하고, 그리고 랜덤으로 나오는 장소가 4개 있다.
-        int ranPoint = Random.Range(0, 9);
+        //0부터 시작하고, 그리고 랜덤으로 나오는 장소가 10개 있다.
+        int ranPoint = Random.Range(0, 10);
 
-        GameObject guestMove = Instantiate(GuestObjs[ranEnemy],
+        GameObject guest = Instantiate(GuestObjs[ranEnemy],
             spawnPoints[ranPoint].position,
             spawnPoints[ranPoint].rotation);
 
-        Rigidbody2D rigid = guestMove.GetComponent<Rigidbody2D>();
-        SpriteRenderer sprite = guestMove.GetComponent<SpriteRenderer>();
-        GuestMove guestLogic = guestMove.GetComponent<GuestMove>();
+        //게스트의 트랜스폼을 content라는 태그를 가진 content를 부모로 삼게 하여 content의 좌표로? 들어간다.
+        guest.transform.SetParent(GameObject.FindGameObjectWithTag("Content").transform, false);
+       
+        //content의 자식으로 생성이 되게 만들었으니 게스트의 좌표는 다시 초기화한다. 
+        guest.transform.localPosition = Vector3.zero;
+        
+        //리시브스폰백터 변수를 이용하여 스폰포인트의 로칼포지션을 받는다.
+        recieveSpawnVector = spawnPoints[ranPoint].localPosition;
+        //게스트에 스폰포인트의 로칼포지션을 넣는다.
+        guest.transform.localPosition = recieveSpawnVector;
+
+
+
+        Rigidbody2D rigid = guest.GetComponent<Rigidbody2D>();
+        SpriteRenderer sprite = guest.GetComponent<SpriteRenderer>();
+        GuestMove guestLogic = guest.GetComponent<GuestMove>();
+
+
         if (ranPoint < 5)
         {
             rigid.velocity = new Vector2(guestLogic.speed  , 0);
