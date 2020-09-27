76a360dac0c59137f589de5c7fbe2ebf4487039c
YerimJung <60317213+YerimJung@users.noreply.github.com>
2020-09-28 08:42:46 +0900

    센터
    제자리점프
    오류수정중

--- Assets/Script/CenterCharMoving.cs
+++ Assets/Script/CenterCharMoving.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,33 +8,50 @@ public class CenterCharMoving : MonoBehaviour
     //고양이 각각의 움직임
     //선행포인트 이벤트 발현
 
-    SpriteRenderer sprite;
-    Vector3 velocity;
-    public float speed=0.01f;
+    //고양이관련변수
+    public SpriteRenderer sprite;
+    public Rigidbody2D catRigid;
+
+    public float speed = 0.1f;
     public bool CanRunRandomize;//이 변수가 true가 되어야 랜덤 움직임 가능
+    Vector3 velocity;
+    Vector2 curPos = Vector2.zero;
+
+    //jump 관련 변수
+    public float jumpheight;
+    bool jumping = false;
 
     void Start()
     {
         velocity = Vector3.zero;
-        sprite = GetComponent<SpriteRenderer>();
     }
 
     void FixedUpdate()
     {
         //고양이 움직임
-        transform.position += velocity.normalized*speed;
+       transform.position += velocity.normalized*speed;
 
         //위아래 경계선 체크
-        if(transform.position.y>=0.24f)
+        if (transform.position.y >= 0.24f)
+        {
             velocity.y *= velocity.y > 0 ? -1 : 1;
-        else if(transform.position.y<=-3.87f)
+        }
+        else if (transform.position.y <= -3.87f)
             velocity.y *= velocity.y > 0 ? 1 : -1;
+
+        /*//점프구간
+        Vector2 cur = transform.localPosition;
+        if (cur.x >= 4100f && cur.x <= 4200f&&cur.y<=-1470f&&cur.y>=-1670f)
+        {
+            StartCoroutine(Jumpp());
+        }*/
 
         //flip
         if (velocity.x >= 0)
             sprite.flipX = true;
         else sprite.flipX = false;
     }
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         //물체와 부딪혔을때 경로 재설정
@@ -49,6 +67,12 @@ public class CenterCharMoving : MonoBehaviour
         //고양이가 캔버스 안에 있을때
         if (collision.name == "Center")
         {
+            Vector2 cur = transform.localPosition;
+            if (cur.x >= 4100f && cur.x <= 4200f && cur.y <= -1470f && cur.y >= -1670f)
+            {
+                StartCoroutine(Jumpp());
+            }
+
             if (CanRunRandomize)
             {
                 //랜덤움직임 무한반복을 막기 위해 시간간격을 준다.
@@ -67,8 +91,6 @@ public class CenterCharMoving : MonoBehaviour
         if (collision.name == "Center")
         {
             float dir = transform.position.x - collision.transform.position.x;
-            //Debug.Log(transform.position.x);
-            //Debug.Log(collision.transform.position.x);
             if (dir >= 0)
             {
                 //고양이가 화면 오른쪽으로 벗어났을때 방향전환
@@ -82,9 +104,45 @@ public class CenterCharMoving : MonoBehaviour
         }
     }
 
+   void jump()
+    {
+        if (!jumping)
+        {
+            jumping = true;
+            curPos = transform.localPosition;
+            catRigid.gravityScale = 1;
+            catRigid.AddForce(Vector2.up * jumpheight, ForceMode2D.Impulse);
+        }
+    }
+
+    void GroundCheck()
+    {
+        if (jumping)
+        {
+            if (transform.localPosition.y<curPos.y)
+            {
+                catRigid.velocity = Vector2.zero;
+                transform.localPosition = new Vector2(curPos.x, curPos.y);
+                catRigid.gravityScale = 0;
+                CanRunRandomize = true;
+                Randomize();
+            }
+        }
+    }
+    IEnumerator Jumpp()
+    {
+        velocity.x = 0f;
+        velocity.y = 0f;
+        CanRunRandomize = false;
+        jump();
+        GroundCheck();
+        yield return new WaitForSeconds(5f);
+        jumping = false;
+    }
+
     IEnumerator Timer()
     {
-        int curtime = 0;
+        int curtime=0;
         while (!CanRunRandomize)
         {
             curtime++;
@@ -104,4 +162,5 @@ public class CenterCharMoving : MonoBehaviour
         velocity.x = randomX;
         velocity.y = randomY;
     }
+
 }
